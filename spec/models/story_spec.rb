1df0f9d2e6cac87256f26fce2f71e6c37457595a
Fred Guest <fredguest@mac.com>
2012-04-25 21:00:26 -0700

    added tests to story model

--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -2,12 +2,33 @@ require 'spec_helper'
 
 describe Story do
   context "when performing validations" do
-  	it "should not be valid without a twitter handle" do
-  		story = Story.new 
-  		story.should be_invalid
+  
+  	let(:story) { Story.new(twitter_handle: "@fredguest", content: "once upon a time") }
   	
+  	it "should be valid with default values" do
+  		story.should be_valid
   	end
   
+  	it "should not be valid without a twitter handle" do
+  		story.twitter_handle = nil 
+  		story.should be_invalid
+  	end
+  	
+  	it "should not be valid without content" do
+  		story.content = nil
+  		story.should be_invalid
+  	end
+  	
+  	it "should not be valid without @ at beginning of twitter handle" do
+  		pending
+  		story.twitter_handle = "twitterdude"	
+  		story.should be_invalid
+  	end
+  	
+  	it "should not be valid with non-word characters in twitter handle" do
+  		pending
+  		story.twitter_handle = "@twitter dude"	
+  		story.should be_invalid
+  	end
   end
-  
 end
