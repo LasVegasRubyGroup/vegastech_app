acb8686c2a2f8c3dc7b47e4cefeaea49da9edb3a
Russ Smith <russ@bashme.org>
2012-07-29 21:33:00 -0700

    No down time unicorn.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,19 +1,32 @@
+rails_env = ENV['RAILS_ENV'] || 'production'
+root_path = File.expand_path(File.dirname(__FILE__) + '/../')
+
 worker_processes 3
 timeout 30
 preload_app true
 
+stderr_path "#{root_path}/log/unicorn.stderr.log"
+stdout_path "#{root_path}/log/unicorn.stdout.log"
+
 before_fork do |server, worker|
   if defined?(ActiveRecord::Base)
     ActiveRecord::Base.connection.disconnect!
-    Rails.logger.info("Disconnected from ActiveRecord")
+    Rails.logger.info('Disconnected from ActiveRecord')
   end
 
-  sleep 1
+  old_pid = "#{root_path}/tmp/pids/unicorn.pid.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill('QUIT', File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
 end
 
 after_fork do |server, worker|
   if defined?(ActiveRecord::Base)
     ActiveRecord::Base.establish_connection
-    Rails.logger.info("Connected to ActiveRecord")
+    Rails.logger.info('Connected to ActiveRecord')
   end
 end
