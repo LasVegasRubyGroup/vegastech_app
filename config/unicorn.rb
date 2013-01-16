70633b742b9ba4e03260a21dcd01b4c4d41bb2d0
Russ Smith <russ@bashme.org>
2013-01-16 11:30:51 -0800

    Uicorn socket path update.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,7 +7,7 @@ preload_app true
 
 pid "#{root_path}/tmp/pids/unicorn.pid"
 
-# listen "#{root_path}/tmp/pids/unicorn.sock", backlog: 2048
+listen "#{root_path}/tmp/sockets/unicorn.sock", backlog: 2048
 
 stderr_path "#{root_path}/log/unicorn.stderr.log"
 stdout_path "#{root_path}/log/unicorn.stdout.log"
