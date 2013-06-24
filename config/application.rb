9a1c56eb25d614ba13842774c0b6aadbcc04926c
Judd Lillestrand <juddl333@gmail.com>
2013-06-24 09:33:30 -0700

    do not initalize on precomplile

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,9 @@ module VegastechApp
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
 
+    #play nice with heroku
+    config.assets.initialize_on_precompile = false
+
     # Enable the asset pipeline
     config.assets.enabled = true
 
