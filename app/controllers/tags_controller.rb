98ac16be5a89c8d4d7b77359fd493f7379e254be
Judd Lillestrand <juddl333@gmail.com>
2013-07-03 10:07:24 -0500

    tags are not showing up

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,7 +2,7 @@ class TagsController < ApplicationController
 
   def show
     @tag = Tag.find(params[:id])
-    @stories = @tag.stories.within_past_month.events_in_the_furture.ranked.page(params[:page]).limit(50)
+    @stories = @tag.stories.within_past_month.ranked.page(params[:page]).limit(50)
   end
 
 end
