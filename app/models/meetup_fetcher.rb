39413f1d86829d7945f5c8060028855bfd5950b4
Judd Lillestrand <juddl333@gmail.com>
2013-07-08 08:50:17 -0700

    only pulll meetup data from 6 weeks out

--- app/models/meetup_fetcher.rb
+++ app/models/meetup_fetcher.rb
@@ -3,7 +3,7 @@ class MeetupFetcher
   require 'json'
 
   def self.get_events
-    meetup_data = JSON.parse(open("https://api.meetup.com/2/open_events.json?text=#{ENV['TRACK_HASHTAG']}&key=#{ENV['MEETUP_KEY']}").read)
+    meetup_data = JSON.parse(open("https://api.meetup.com/2/open_events.json?text=#{ENV['TRACK_HASHTAG']}&time=0,6w&key=#{ENV['MEETUP_KEY']}").read)
 
     meetup_data['results'].each do |event|
       Story.find_or_create_by_meetup_id(event)
